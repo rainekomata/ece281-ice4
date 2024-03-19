e521b1e872de0fc0ecb273da04ec50674917a95c
rainekomata <156048795+rainekomata@user.noreply.github.com.>
2024-03-19 14:19:32 -0600

    ICE4

--- src/hdl/top_basys3.vhd
+++ src/hdl/top_basys3.vhd
@@ -11,8 +11,8 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : top_basys3.vhd
---| AUTHOR(S)     : Capt Phillip Warner
---| CREATED       : 02/22/2018 Modified: 03/01/2020 by capt Dan Johnson
+--| AUTHOR(S)     : Raine Komata
+--| CREATED       : 03/14/2024 Modified: 03/01/2020 by capt Dan Johnson
 --| DESCRIPTION   : This file implements the top level module for the solution for Stoplight FSM.
 --|
 --|					Inputs:  clk 	--> 100 MHz clock from FPGA
@@ -70,7 +70,15 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
 
 --Declare stoplight component here 
-
+component stoplight_fsm is 
+    Port (i_C        : in std_logic;
+            i_clk    : in std_logic;
+            i_reset  : in std_logic;
+            o_R      : out std_logic;
+            o_Y      : out std_logic;
+            o_G      : out std_logic 
+         );
+end component stoplight_fsm;
 
 component clock_divider is
 	generic ( constant k_DIV : natural := 2	);
@@ -85,15 +93,22 @@ end component clock_divider;
 begin
 	-- PORT MAPS ----------------------------------------
 	--Port map stoplight here based on the design provided
-
-
+stoplight_fsm_inst: stoplight_fsm
+port map (
+            i_C => sw(0),
+            i_Reset => btnC,
+            i_clk => w_clk,
+            o_R => JA(0),
+            o_Y => JA(1),
+            o_G => JA(2)
+          );
 --Complete the clock_divider portmap below based on the design provided	
 	clkdiv_inst : clock_divider 		--instantiation of clock_divider to take 
         generic map ( k_DIV => 50000000 ) -- 1 Hz clock from 100 MHz
         port map (						  
-            i_clk   => 
-            i_reset => 
-            o_clk   => 
-        );    
+                    i_clk   => clk,
+                    i_reset => btnL,
+                    o_clk   => w_clk
+                  );    
 	
 end top_basys3_arch;
